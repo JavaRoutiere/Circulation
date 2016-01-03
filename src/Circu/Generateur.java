550ee20a3971a7c06327a036d02917905c223c82
Lahat <bamba34@hotmail.com>
2016-01-03 23:52:11 +0100

    2iem maj

--- src/Circu/Generateur.java
+++ src/Circu/Generateur.java
@@ -7,40 +7,30 @@ public class Generateur extends Thread{
 	static boolean fini;
 	private Random rnd = new Random();
 	private int maxIntervalle;
-<<<<<<< HEAD
 	static final int port = 6050;
 	InetAddress adresse;
 	boolean voieBasse;
 	
-	public Generateur(boolean voieBasse){
-=======
-	boolean voieBasse;
 	
 	public Generateur(){}
 	public Generateur(boolean c){
->>>>>>> 1816d193cb35aab928f8dd3b0bc2135e061e337c
 		fini = false;
 		maxIntervalle = 1700;
 		voieBasse = c;
 		rnd = new Random();
-<<<<<<< HEAD
 		maxIntervalle = 1700;
 		this.voieBasse = voieBasse;
-=======
->>>>>>> 1816d193cb35aab928f8dd3b0bc2135e061e337c
 	}
 	
 	public void run(){
 		while(!fini){
 			Vehicule v = new Vehicule();
-<<<<<<< HEAD
 			//TODO ?????????
 			if(!voieBasse)
 				Vehicule.VH.put(60, v);
 			
 			else
 				Vehicule.VB.put(-60, v);
-=======
 			if(voieBasse){
 				Vehicule.VB.put(-60, v);
 				try {
@@ -59,7 +49,6 @@ public class Generateur extends Thread{
 				}
 			}
 			
->>>>>>> 1816d193cb35aab928f8dd3b0bc2135e061e337c
 			
 			v.start();
 			try {
